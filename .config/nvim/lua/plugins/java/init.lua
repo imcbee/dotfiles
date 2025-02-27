794c2feaea770cdfe45b8dee3227a3bc4ea90c95
Ian McBee <ian.mcbee@blackcape.io>
2025-02-27 08:12:25 -0500

    update nvim-java for BC

--- .config/nvim/lua/plugins/java/init.lua
+++ .config/nvim/lua/plugins/java/init.lua
@@ -8,34 +8,38 @@ return {
         servers = {
           -- Your JDTLS configuration goes here
           jdtls = {
-            -- settings = {
-            --   java = {
-            --     configuration = {
-            --       runtimes = {
-            --         {
-            --           name = "JavaSE-23",
-            --           path = "/usr/local/sdkman/candidates/java/23-tem",
-            --         },
-            --       },
-            --     },
-            --   },
-            -- },
+            settings = {
+              java = {
+                configuration = {
+                  runtimes = {
+                    {
+                      name = "openjdk 21.0.2 2024-01-16",
+                      path = "/Users/ianmcbee/.asdf/shims/java",
+                    },
+                  },
+                },
+              },
+            },
           },
         },
         setup = {
           jdtls = function()
             -- Your nvim-java configuration goes here
             require("java").setup({
-              -- root_markers = {
-              --   "settings.gradle",
-              --   "settings.gradle.kts",
-              --   "pom.xml",
-              --   "build.gradle",
-              --   "mvnw",
-              --   "gradlew",
-              --   "build.gradle",
-              --   "build.gradle.kts",
-              -- },
+              root_markers = {
+                "settings.gradle",
+                "settings.gradle.kts",
+                "pom.xml",
+                "build.gradle",
+                "mvnw",
+                "gradlew",
+                "build.gradle",
+                "build.gradle.kts",
+              },
+              notifications = {
+                -- enable 'Configuring DAP' & 'DAP configured' messages on start up
+                dap = true,
+              },
             })
           end,
         },
