d0cf712a2c28ec33b2bda00ac94e2bc308bb97d7
Ian McBee <ian.mcbee@blackcape.io>
2025-03-11 14:47:18 -0400

    update nvim config and add plugins

--- .config/nvim/lua/plugins/treesitter/init.lua
+++ .config/nvim/lua/plugins/treesitter/init.lua
@@ -2,5 +2,6 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     ensure_installed = { "java", "graphql", "sql", "css", "scss", "dockerfile", "tsx" },
+    indent = { enable = true, disable = { "html" } },
   },
 }
