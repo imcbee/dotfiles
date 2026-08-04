94c835da52612c4689345e5b3a4040134c5bd951
Ian McBee <ian.mcbee@blackcape.io>
2026-08-03 22:57:25 -0400

    update ideavimrc and add bash to treesitter

--- nvim/.config/nvim/lua/plugins/treesitter/init.lua
+++ nvim/.config/nvim/lua/plugins/treesitter/init.lua
@@ -1,7 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { "java", "graphql", "sql", "css", "scss", "dockerfile", "tsx", "python", "zsh" },
+    ensure_installed = { "java", "graphql", "sql", "css", "scss", "dockerfile", "tsx", "python", "zsh", "bash" },
     indent = { enable = true, disable = { "html" } },
   },
 }
